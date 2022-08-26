9c7d7795aa8717a72eaccc15c7f8fee09eb61925
kamdem2240 <111396590+kamdem2240@users.noreply.github.com>
2022-08-26 17:25:53 -0400

    Update english.sh

--- english.sh
+++ english.sh
@@ -1,1 +1,22 @@
 english comprehension 
+mazon Web Services - Cloud Computing with AWS
+Ad·
+https://aws.amazon.com/account/sign-up
+Build, Deploy, and Manage Websites, Apps or Processes on AWS' Secure, Reliable Network. Sign Up for a Free Account & Experience AWS' Secure, Reliable, Scalable Services. Secure Solutions. HIPAA Compliant. Highly Scalable. Easily Manage Clusters.
+AWS Training
+Access 500+ Free Digital Courses Built by AWS Experts.
+Amazon Web Services FAQs
+Learn How AWS Can Help Lower Costs & Innovate Faster. Free For 1 Year.
+Current AWS Customers
+Get the Most Out of AWS Cloud. Pick a Learning Path & Get Started.
+
+Microsoft® Azure Cloud - Get Your Free Account
+Ad·
+https://azure.microsoft.com/account/free
+Move Faster, Achieve More and Save Money with Azure. Get Started Today! Get Free Credit.
+‎Azure Pricing Calculator · ‎What is Azure? · ‎Azure for Open Source · ‎Sign Up With GitHub
+
+Try It Free For 30 Days - Schedule Your Free Demo Today
+Ad·
+https://www.quickbase.com/compare/ms-access
+Support Thousands Of Users With No Performance Slowdowns. Try It Free For 30 Days.
