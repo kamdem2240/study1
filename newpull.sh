ec0a400ee62d64d8b7d6a4c1aa566ed4c86dd104
kamdem2240 <111396590+kamdem2240@users.noreply.github.com>
2022-08-26 17:35:09 -0400

    Create newpull.sh

---
+++ newpull.sh
@@ -0,0 +1,11 @@
+Topic: Patrick Kamdem's Personal Meeting Room
+
+Join Zoom Meeting
+https://us06web.zoom.us/j/4487140708?pwd=lwReiY78a7eT_l9hO7TIbrh3k5CbQy.1
+
+Meeting ID: 448 714 0708
+Passcode: 01eQC8
+
+Zoom Video (https://us06web.zoom.us/j/4487140708?pwd=lwReiY78a7eT_l9hO7TIbrh3k5CbQy.1)
+Join our Cloud HD Video Meeting
+Zoom is the leader in modern enterprise video communications, with an easy, reliable cloud platform for video and audio conferencing, chat, and webinars across mobile, desktop, and room systems. Zoom Rooms is the original software-based conference room solution used around the world in board, conference, huddle, and training rooms, as well as executive offices and classrooms. Founded in 2011, Zoom helps businesses and organizations bring their teams together in a frictionless environment to get more done. Zoom is a publicly traded company headquartered in San Jose, CA.
